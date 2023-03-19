1cc8596785ad9c77857f969586a7ba79bd628bd3
dilkash7 <dilkash.3mindsdigital@gmail.com>
2023-03-19 13:12:56 +0530

    components changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import PaymentSuccessFull from './PaymentSuccessFull';
+
 import Products from './Products';
 function App() {
 	return (
 		<div className='App px-10'>
 			<Routes>
 				<Route path='/' element={<Products />} />
-				<Route
-					path='/paymentsuccess'
-					element={<PaymentSuccessFull />}
-				/>
-				<Route path='/dashboard' element={<Dashboard />} />
 			</Routes>
 		</div>
 	);
--- src/PaymentSuccessFull.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PaymentSuccessFull = () => {
-	return <div>PaymentSuccessFull</div>;
-};
-
-export default PaymentSuccessFull;
